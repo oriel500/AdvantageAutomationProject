0e7fa670799bb96c3bb272b9b9255348956dc13c
DELL <orielnaim2001@gmail.com>
2022-11-09 18:17:24 +0200

    fix Advantage_OrderPayment_Actions.py

--- MainPackage/ObjectsPages/Advantage_OrderPayment_Actions.py
+++ MainPackage/ObjectsPages/Advantage_OrderPayment_Actions.py
@@ -4,6 +4,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# from selenium.webdriver.chrome.service import Service
+# from time import sleep
+# from Advantage_SignInPopUp import Advantage_SignInPopUp
+# from Advantage_ToolBar import Advantage_ToolBar
+# from Advantage_AccountOperationsMenu import Advantage_AccountOperationsMenu
 
 class Advantage_OrderPayment_Actions:
     def __init__(self, _driver: webdriver.Chrome):
@@ -45,11 +50,9 @@ class Advantage_OrderPayment_Actions:
         return self.driver.find_element(By.CSS_SELECTOR, "input[name='masterCredit'")
 
     def click_safepay(self):
-        self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='safepay']")))
         self.safepay_radio_button().click()
 
     def click_master_credit(self):
-        self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='masterCredit']")))
         self.master_credit_radio_button().click()
 
     def safepay_username_editbox(self):
@@ -76,17 +79,24 @@ class Advantage_OrderPayment_Actions:
 
     # string from thank you page
     def thank_you_text(self):
+        # wait until locate the text "Thank you..."
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "h2>span")))
         return self.driver.find_element(By.CSS_SELECTOR, "h2>span").text
 
     # tracking number from thank you page
     def tracking_number_from_thankYou_page(self):
+        # wait until locate the text "Thank you..."
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "h2>span")))
         return self.driver.find_element(By.ID, "trackingNumberLabel").text
 
     # order number from thank you page
     def order_number_from_thankYou_page(self):
+        # wait until locate the text "Thank you..."
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "h2>span")))
         return self.driver.find_element(By.ID, "orderNumberLabel").text
 
     def get_orders_table(self):
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, "//*[text()='MY ORDERS']")))
         return self.driver.find_element(By.CSS_SELECTOR, "div.cover>table")
 
     # get only the rows with order number
@@ -105,7 +115,51 @@ class Advantage_OrderPayment_Actions:
 
     # get the id of order by index of rows in the table
     def get_id_order_by_index(self, index):
+        # get row of order
         order = self.get_order_by_index(index)
+        # get columns of row
         order_cols = order.find_elements(By.TAG_NAME, "td")
+        # get id from column 0
         order_id = order_cols[0].find_element(By.TAG_NAME, "label").text
         return order_id
+
+    # get the id of last order added
+    def get_id_last_order(self):
+        # get row of order
+        order = self.get_order_by_index(-1)
+        # get columns of row
+        order_cols = order.find_elements(By.TAG_NAME, "td")
+        # get id from column 0
+        order_id = order_cols[0].find_element(By.TAG_NAME, "label").text
+        return order_id
+
+
+# === Check if the class work ===
+# Setup
+# service = Service(r"C:\seleniumQA7\chromedriver.exe")
+# driver_chrome = webdriver.Chrome(service=service)
+# driver_chrome.get("https://advantageonlineshopping.com/#/")
+# driver_chrome.implicitly_wait(30)
+# driver_chrome.maximize_window()
+# toolbar = Advantage_ToolBar(driver_chrome)
+# sign_in_page = Advantage_SignInPopUp(driver_chrome)
+# orders_actions = Advantage_OrderPayment_Actions(driver_chrome)
+# account_menu = Advantage_AccountOperationsMenu(driver_chrome)
+#
+# toolbar.click_user()
+# sign_in_page.sign_in("test0001", "Aabc12")
+# toolbar.click_cart_checkout_popup()
+# sleep(1)
+# orders_actions.click_next()
+# sleep(2)
+# orders_actions.click_master_credit()
+# sleep(2)
+# orders_actions.pay_now_by_safepay("Test0001", "Aabc12")
+# print(orders_actions.order_number_from_thankYou_page())
+# toolbar.click_user()
+# account_menu.click_my_orders()
+# sleep(1)
+# print(orders_actions.get_id_last_order())
+# sleep(10)
+
+
